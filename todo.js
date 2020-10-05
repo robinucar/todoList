457379e3a00dd40c0e2ba1065b559a5135eec73f
Robin <mehmet.ucar@imediapro.co.uk>
2020-10-05 21:17:16 +0100

    class base

--- todo.js
+++ todo.js
@@ -1,3 +1,4 @@
+/*
 const todoList = [];
 
 const todoListElement = document.querySelector('#myUL');
@@ -42,3 +43,65 @@ function displayTodo() {
     todoListElement.appendChild(listElement);
   });
 }
+*/
+//class base
+
+const todoList = [];
+
+class TodoList {
+  constructor(listElementParam) {
+    this.todoListElement = listElementParam;
+  }
+
+  add(todoText) {
+    const todoObject = {
+      // id: todoList.length * 5,
+      id: todoList.length,
+      todoText: todoText,
+      isDone: false,
+    };
+
+    todoList.push(todoObject);
+    this.display();
+  }
+
+  done(todoId) {
+    const selectedTodoIndex = todoList.findIndex((item) => item.id == todoId);
+    todoList[selectedTodoIndex].isDone = true;
+
+    this.display();
+  }
+
+  display() {
+    this.todoListElement.innerHTML = "";
+
+    todoList.forEach((item) => {
+      const listElement = document.createElement("li");
+
+      listElement.innerText = item.todoText;
+      listElement.setAttribute("data-id", item.id);
+
+      if (item.isDone) {
+        listElement.classList.add("checked");
+      }
+
+      listElement.addEventListener("click", function (e) {
+        const selectedId = e.target.getAttribute("data-id");
+        myTodoList.done(selectedId);
+      });
+
+      this.todoListElement.appendChild(listElement);
+    });
+  }
+}
+
+const listSection = document.querySelector("#myUL");
+
+
+const myTodoList = new TodoList(listSection);
+
+document.querySelector("#todo_button").addEventListener("click", function () {
+  const todoText = document.querySelector("#myInput").value;
+
+  myTodoList.add(todoText);
+});
